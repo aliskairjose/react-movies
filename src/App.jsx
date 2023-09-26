508ccab11254e5780a6942278af7fcf0c9e3c16f
Kervin Gonzalez <kervingonzalez@gmail.com>
2023-09-26 12:31:37 -0400

    MU Library

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-
-import './App.css'
+import "./App.css";
+import * as React from "react";
+import Button from "@mui/material/Button";
 function App() {
-
   return (
     <>
-     <p>Movie wweb</p>
+      <p>Movie wweb</p>
+      <Button variant="text">Text</Button>
+<Button variant="contained">Contained</Button>
+<Button variant="outlined">Outlined</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
