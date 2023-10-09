c36619ee98a3b649c41b7ac89c7751134ad8f1c6
Kervin Gonzalez <kervingonzalez@gmail.com>
2023-10-09 12:20:22 -0400

    ajustes

--- src/routes/rutas.jsx
+++ src/routes/rutas.jsx
@@ -14,8 +14,8 @@ import Company from "../pages/Company";
 const rutas = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route exact path="/react-movies" element={<App />}>
-        <Route exact path="/react-movies" element={<Home />} />
+      <Route exact path="/react-movies/" element={<App />}>
+        <Route exact path="/react-movies/" element={<Home />} />
         <Route exact path="/react-movies/results" element={<Result />} />
         <Route exact path="/react-movies/detail/:mediaType/:id" element={<Detail />} />
         <Route exact path="/react-movies/detail/:mediaType/:id/credits" element={<CRedits />} />
